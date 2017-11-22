9f8539c38ece1f5044b9b67ed061b538f5078143
coz <chaonhoiso@gmail.com>
2017-11-22 18:53:14 +0700

    a

--- src/screen/curriculum/CurriculumInformationContainer.js
+++ src/screen/curriculum/CurriculumInformationContainer.js
@@ -4,7 +4,7 @@ import {
 } from 'react-native';
 import {
     Body, CardItem, Header, Container, Button,
-    Left, Right, Spinner, Item
+    Left, Right, Spinner, Item,
 } from 'native-base';
 import BackButton from '../../commons/BackButton';
 import Icon from '../../commons/Icon';
@@ -34,6 +34,7 @@ class CurriculumInformationContainer extends Component {
         };
         this.onLoad = this.onLoad.bind(this);
         this.onProgress = this.onProgress.bind(this);
+        this.onEnd = this.onEnd.bind(this);
     }
 
     componentWillMount() {
@@ -77,6 +78,10 @@ class CurriculumInformationContainer extends Component {
                 paused: true
             })
         }
+    }
+
+    onEnd(){
+        alert("AASF");
     }
 
     render() {
@@ -174,98 +179,102 @@ class CurriculumInformationContainer extends Component {
                             activeOpacity={0.8}
                             style={part.imageLesson}
                         >
-                            <View
-                                style={
-                                    [
-                                        part.imageLesson,
-                                        Platform.OS === 'ios'
-                                            ?
-                                            part.shadow
-                                            :
-                                            ''
-                                    ]
-                                }
-                            >
-                                <Image
-                                    source={{uri: data.audio_url }}
-                                    style={part.imageLesson}
+                            {
+                                isLoadingCurriculum
+                                    ?
+                                    <View style={[part.wrapperContainer, {height : 80}]}>
+                                        <Spinner color={color.gray}/>
+                                    </View>
+                                    :
+                                    <View
+                                        style={
+                                            [
+                                                part.imageLesson,
+                                                Platform.OS === 'ios'
+                                                    ?
+                                                    part.shadow
+                                                    :
+                                                    ''
+                                            ]
+                                        }
+                                    >
+                                        <Image
+                                            source={{uri: data.image_url}}
+                                            style={part.imageLesson}
+                                        />
+                                    </View>
+                            }
+
+                        </TouchableOpacity>
+                    </View>
+                </ParallaxScrollView>
+                {
+                    isLoadingCurriculum
+                        ?
+                        <View/>
+                        :
+                        <View style={[part.bottomModalInLesson, part.haveBorderTop]}>
+                            <View style={{flexDirection: 'row'}}>
+                                <TouchableOpacity
+                                    activeOpacity={1}
+                                    style={[part.padding, part.wrapperButtonPlay]}
+                                    onPress={() => this.playSound()}
+                                >
+                                    <Icon name={iconPlay}
+                                          size={25}
+                                          color={color.navTitle}
+                                    />
+                                </TouchableOpacity>
+                                <View style={[{flex: 1, justifyContent: 'center'}, part.marginLeft]}>
+                                    <Text style={part.titleSmallDarkBold} numberOfLines={1}>
+                                        {
+                                            data.name ? data.name.toUpperCase() : data.name
+                                        }
+                                    </Text>
+                                    <Text style={part.describeDarkGray} numberOfLines={1}>
+                                        {
+                                            data.name
+                                        }
+                                    </Text>
+                                </View>
+                                <Video
+                                    source={{uri: "https://api.soundcloud.com/tracks/47893729/stream?client_id=3YnCkFCcm5cBfYqlXr3ufGY7k2izG1lv"}}   // Can be a URL or a local file.
+                                    rate={play}                              // 0 is paused, 1 is normal.
+                                    volume={1}                            // 0 is muted, 1 is normal.
+                                    muted={false}                           // Mutes the audio entirely.
+                                    paused={paused}                          // Pauses playback entirely.
+                                    repeat={false}                           // Repeat forever.
+                                    playInBackground={false}                // Audio continues to play when app entering background.
+                                    playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+                                    ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+                                    progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+                                    onLoad={this.onLoad}    // Callback when video loads
+                                    onProgress={this.onProgress}    // Callback every ~250ms with currentTime
+                                    onEnd={this.onEnd}
                                 />
                             </View>
-                        </TouchableOpacity>
-                    </View>
-
-
-                </ParallaxScrollView>
-                <View style={[part.bottomModalInLesson, part.haveBorderTop]}>
-                    <View style={{flexDirection: 'row'}}>
-                        <TouchableOpacity
-                            activeOpacity={1}
-                            style={[part.padding, part.wrapperButtonPlay]}
-                            onPress={() => this.playSound()}
-                        >
-                            <Icon name={iconPlay}
-                                  size={25}
-                                  color={color.navTitle}
-                            />
-                        </TouchableOpacity>
-                        <View style={[{flex: 1, justifyContent: 'center'}, part.marginLeft]}>
-                            <Text style={part.titleSmallDarkBold} numberOfLines={1}>
-                                {
-                                    isLoadingCurriculum
-                                        ?
-                                        'Đang tải...'
-                                        :
-                                        data.name ? data.name.toUpperCase() : data.name
-                                }
-                            </Text>
-                            <Text style={part.describeDarkGray} numberOfLines={1}>
-                                {
-                                    isLoadingCurriculum
-                                        ?
-                                        'Đang tải...'
-                                        :
-                                        data.name
-                                }
-                            </Text>
-                        </View>
-                        <Video
-                            source={{uri: data.audio_url}}   // Can be a URL or a local file.
-                            ref={(ref) => {
-                                this.player = ref
-                            }}                                      // Store reference
-                            rate={play}                              // 0 is paused, 1 is normal.
-                            volume={1}                            // 0 is muted, 1 is normal.
-                            muted={false}                           // Mutes the audio entirely.
-                            paused={paused}                          // Pauses playback entirely.
-                            repeat={false}                           // Repeat forever.
-                            playInBackground={true}                // Audio continues to play when app entering background.
-                            playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
-                            ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
-                            progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
-                            onLoad={this.onLoad}    // Callback when video loads
-                            onProgress={this.onProgress }    // Callback every ~250ms with currentTime
-                        />
-                    </View>
-                    <View style={{paddingTop: 10}}>
-                        <View style={part.wrapperDeadline}>
-                            <View
-                                style={[part.deadlineProgress, {width: widthDeadlineProgress}]}>
+                            <View style={{paddingTop: 10}}>
+                                <View style={part.wrapperDeadline}>
+                                    <View
+                                        style={[part.deadlineProgress, {width: widthDeadlineProgress}]}>
+                                    </View>
+                                </View>
+                            </View>
+                            <View style={[part.paddingTop, {
+                                width: size.wid - 20,
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                            }]}>
+                                <Text style={part.describeDarkGray}>
+                                    {this.state.minute}:{this.state.second < 10 ? '0' : ''}{this.state.second}
+                                </Text>
+                                <Text style={part.describeDarkGray}>
+                                    {parseInt(this.state.duration / 60)}:{parseInt(this.state.duration % 60) < 10 ? '0' : ''}{parseInt(this.state.duration % 60)}
+                                </Text>
                             </View>
                         </View>
-                    </View>
-                    <View style={[part.paddingTop, {
-                        width: size.wid - 20,
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                    }]}>
-                        <Text style={part.describeDarkGray}>
-                            {this.state.minute}:{this.state.second < 10 ? '0' : ''}{this.state.second}
-                        </Text>
-                        <Text style={part.describeDarkGray}>
-                            {parseInt(this.state.duration / 60)}:{parseInt(this.state.duration % 60) < 10 ? '0' : ''}{parseInt(this.state.duration % 60)}
-                        </Text>
-                    </View>
-                </View>
+
+                }
             </Container>
         );
 
