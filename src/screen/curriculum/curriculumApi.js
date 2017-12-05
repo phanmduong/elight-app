8acee697f1e24f107d96d6471dc9303d027fe947
phanmduong <phanminhduong@hotmail.com>
2017-12-05 20:18:46 +0700

    update code

--- src/screen/curriculum/curriculumApi.js
+++ src/screen/curriculum/curriculumApi.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
 import * as env from '../../constants/env'
+
+const tokenGlobal = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjU1NDksImlzcyI6Imh0dHA6Ly9hcGkua2VldG9vbDMueHl6L2xvZ2luIiwiaWF0IjoxNTEyNDc4MDY3LCJleHAiOjE1MTMwODI4NjcsIm5iZiI6MTUxMjQ3ODA2NywianRpIjoiYURiMnV2T3JyZkRNVFQ0QiJ9.yvu0B_Bjo3axQpWndnxQAnMdzHwbzfL5Vs-KQUoP330';
 export function curriculumApi(value) {
-    let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjU1NDksImlzcyI6Imh0dHA6Ly9hcGkua2VldG9vbDMueHl6L2xvZ2luIiwiaWF0IjoxNTExMjM3MDUyLCJleHAiOjE1MTE4NDE4NTIsIm5iZiI6MTUxMTIzNzA1MiwianRpIjoiRnB6aGRMb094aDE1WFNIVyJ9.MEooKSXvepDmFN95CSB8dwU3W6js99eXDTdvj83RKrc';
+    let token = tokenGlobal;
     let url = env.API_COLORME + '/lesson/' + value + '?token=' + token;
     return axios.get(url);
 }
 
 export function courseApi() {
-    let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjU1NDksImlzcyI6Imh0dHA6Ly9hcGkua2VldG9vbDMueHl6L2xvZ2luIiwiaWF0IjoxNTExMjM3MDUyLCJleHAiOjE1MTE4NDE4NTIsIm5iZiI6MTUxMTIzNzA1MiwianRpIjoiRnB6aGRMb094aDE1WFNIVyJ9.MEooKSXvepDmFN95CSB8dwU3W6js99eXDTdvj83RKrc';
+    let token = tokenGlobal;
     let url = env.API_COLORME+ "/paid-courses?token=" + token;
     return axios.get(url);
 }
 
 export function resourcesApi(class_name) {
-    let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjU1NDksImlzcyI6Imh0dHA6Ly9hcGkua2VldG9vbDMueHl6L2xvZ2luIiwiaWF0IjoxNTExMjM3MDUyLCJleHAiOjE1MTE4NDE4NTIsIm5iZiI6MTUxMTIzNzA1MiwianRpIjoiRnB6aGRMb094aDE1WFNIVyJ9.MEooKSXvepDmFN95CSB8dwU3W6js99eXDTdvj83RKrc';
+    let token = tokenGlobal;
     let url = env.API_COLORME + '/links/' + class_name + '?token=' + token;
     return axios.get(url);
 
